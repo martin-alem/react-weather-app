9523d089e15c4f548a296b93b223dd3f1c795ea1
martin-alem <alemajohmartin@gmail.com>
2021-05-16 10:29:26 -0400

    implement weather manager component and add sub components

--- src/components/WeatherManager/WeatherManager.js
+++ src/components/WeatherManager/WeatherManager.js
@@ -2,6 +2,10 @@ import React from 'react';
 import "./WeatherManager.css";
 import "./background-image.jpg";
 import SearchForm from '../forms/SearchForm';
+import DefaultButton from '../buttons/DefaultButton';
+import LocationButton from '../buttons/LocationButton';
+import BookMarkButton from '../buttons/BookMarkButton';
+import SaveButton from '../buttons/SaveButton';
 
 class WeatherManager extends React.Component {
     constructor(props) {
@@ -13,7 +17,26 @@ class WeatherManager extends React.Component {
             <React.Fragment>
                 <div className="WeatherManager">
                     <div className="WeatherManager-controls">
-                        <SearchForm />
+                        <div className="search-form">
+                            <SearchForm />
+                        </div>
+                        <div className="default-btn">
+                            <DefaultButton className="default-btn" />
+                        </div>
+
+                        <div className="location-btn">
+                            <LocationButton className="location-btn" />
+                        </div>
+
+                        <div className="bookmark-btn">
+                            <BookMarkButton />
+                        </div>
+
+
+                        <div className="save-btn">
+                            <SaveButton />
+                        </div>
+
                     </div>
                 </div>
             </React.Fragment>
